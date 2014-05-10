e74f48015f4a192c7f82f639a12835fccdc2f51d
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-05-09 23:57:46 -0400

    blah

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $footer = $('#footer');
   $footer.hover(function() {
   	$footer.animate({height: "+=30vh"}, 500)
-  	$footer.html('<div>Contact</div><div><ul><li>About Me</li><li>Previous Work</li><a href="onlineresume.docx"><li>Resume</li></a></ul></div>');
+  	$footer.html('<div>Contact</div><div><ul><li>About Me</li><li>Previous Work</li><a href="onlineresume.pdf"><li>Resume</li></a></ul></div>');
   },
   function() {
   	$footer.animate({height: "-=30vh"}, 500);
