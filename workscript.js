8eba93bf355b75065e4cc77f3cec2b8570fabbbb
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-09-17 20:47:50 -0400

    Workscript fixes

--- workscript.js
+++ workscript.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   var workActivated = 'none';
 
   $('#stocks').click(function() {
-    $('#content-container').fadeOut(200);
-    setTimeout(function() {$('#content-container').load('stocks.html');}, 200);
-    $('#content-container').fadeIn(200);
     if (workActivated!='stocks') {
+      $('#content-container').fadeOut(200);
+      setTimeout(function() {$('#content-container').load('stocks.html');}, 200);
+      $('#content-container').fadeIn(200);
       $('.work-nav-left').css('background-color', '#AAAAAA');
       $('.work-nav-left').animate({ width: '23vw'}, 200);
       $(this).css('background-color', '#999999');
@@ -15,10 +15,10 @@ $(document).ready(function() {
   });
 
   $('#robot').click(function() {
-    $('#content-container').fadeOut(200);
-    setTimeout(function() {$('#content-container').load('carl.html');}, 200);
-    $('#content-container').fadeIn(200);
     if (workActivated!='robot') {
+      $('#content-container').fadeOut(200);
+      setTimeout(function() {$('#content-container').load('carl.html');}, 200);
+      $('#content-container').fadeIn(200);
       $('.work-nav-left').css('background-color', '#AAAAAA');
       $('.work-nav-left').animate({ width: '23vw'}, 200);
       $(this).css('background-color', '#999999');
@@ -28,10 +28,10 @@ $(document).ready(function() {
   });
 
   $('#basketball').click(function() {
-    $('#content-container').fadeOut(200);
-    setTimeout(function() {$('#content-container').load('basketball.html');}, 200);
-    $('#content-container').fadeIn(200);
     if (workActivated!='basketball') {
+      $('#content-container').fadeOut(200);
+      setTimeout(function() {$('#content-container').load('basketball.html');}, 200);
+      $('#content-container').fadeIn(200);
       $('.work-nav-left').css('background-color', '#AAAAAA');
       $('.work-nav-left').animate({ width: '23vw'}, 200);
       $(this).css('background-color', '#999999');
@@ -41,10 +41,10 @@ $(document).ready(function() {
   });
 
   $('#breadboard').click(function() {
-    $('#content-container').fadeOut(200);
-    setTimeout(function() {$('#content-container').load('breadboard.html');}, 200);
-    $('#content-container').fadeIn(200);
     if (workActivated!='breadboard') {
+      $('#content-container').fadeOut(200);
+      setTimeout(function() {$('#content-container').load('breadboard.html');}, 200);
+      $('#content-container').fadeIn(200);
       $('.work-nav-left').css('background-color', '#AAAAAA');
       $('.work-nav-left').animate({ width: '23vw'}, 200);
       $(this).css('background-color', '#999999');
@@ -54,10 +54,10 @@ $(document).ready(function() {
   });
 
   $('#2048').click(function() {
-    $('#content-container').fadeOut(200);
-    setTimeout(function() {$('#content-container').load('2048.html');}, 200);
-    $('#content-container').fadeIn(200);
     if (workActivated!='2048') {
+      $('#content-container').fadeOut(200);
+      setTimeout(function() {$('#content-container').load('2048.html');}, 200);
+      $('#content-container').fadeIn(200);
       $('.work-nav-left').css('background-color', '#AAAAAA');
       $('.work-nav-left').animate({ width: '23vw'}, 200);
       $(this).css('background-color', '#999999');
@@ -67,10 +67,10 @@ $(document).ready(function() {
   });
 
   $('#chess').click(function() {
-    $('#content-container').fadeOut(200);
-    setTimeout(function() {$('#content-container').load('chess.html');}, 200);
-    $('#content-container').fadeIn(200);
     if (workActivated!='chess') {
+      $('#content-container').fadeOut(200);
+      setTimeout(function() {$('#content-container').load('chess.html');}, 200);
+      $('#content-container').fadeIn(200);
       $('.work-nav-left').css('background-color', '#AAAAAA');
       $('.work-nav-left').animate({ width: '23vw'}, 200);
       $(this).css('background-color', '#999999');
@@ -80,10 +80,10 @@ $(document).ready(function() {
   });
 
   $('#chatbot').click(function() {
-    $('#content-container').fadeOut(200);
-    setTimeout(function() {$('#content-container').load('chatbot.html');}, 200);
-    $('#content-container').fadeIn(200);
     if (workActivated!='chatbot') {
+      $('#content-container').fadeOut(200);
+      setTimeout(function() {$('#content-container').load('chatbot.html');}, 200);
+      $('#content-container').fadeIn(200);
       $('.work-nav-left').css('background-color', '#AAAAAA');
       $('.work-nav-left').animate({ width: '23vw'}, 200);
       $(this).css('background-color', '#999999');
