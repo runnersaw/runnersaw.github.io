31c120c6dd83de0b8ed392a3e23e547cbba0056c
Sawyer Vaughan <sawyervaughan@gmail.com>
2018-12-02 12:52:12 -0800

    Add Secret Santas for the party

--- christmas/generate_secret_santa.py
+++ christmas/generate_secret_santa.py
@@ -10,9 +10,6 @@ def _verify_list(people_pairs):
     for pair in people_pairs:
         person = pair[0]
         target = pair[1]
-	print(person.name)
-	print(target.name)
-	print(person.exceptions)
 	if target.name in person.exceptions:
 	    return False
     return True
@@ -44,11 +41,19 @@ def write_files(pairs):
 if __name__=="__main__":
     people = []
     people.append(Person("Sawyer", exceptions=["Giulia"]))
-    people.append(Person("Giulia", exceptions=["Chris"]))
+    people.append(Person("Giulia", exceptions=["Sawyer"]))
     people.append(Person("Chris"))
+    people.append(Person("Luke", exceptions=["Maria"]))
+    people.append(Person("Maria", exceptions=["Luke"]))
+    people.append(Person("Katya", exceptions=["Patrick"]))
+    people.append(Person("Patrick", exceptions=["Katya"]))
+    people.append(Person("Walker"))
+    people.append(Person("Keenan"))
+    people.append(Person("Andrew"))
+    people.append(Person("Logan"))
     pairs = create_pairs(people)
     names = map(lambda x: (x[0].name, x[1].name), pairs)
-    print(names)
+    # print(names)
 
     write_files(pairs)
 
